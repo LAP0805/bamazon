2a0ddac0a059a965333aa5bb95719f7bf908d1aa
Janxxy <lesley.pond@gmail.com>
2019-03-05 17:54:43 -0500

    supervisor

--- supervisor.js
+++ supervisor.js
@@ -3,6 +3,7 @@ const inquirer = require("inquirer");
 const mysql = require("mysql");
 const chalk = require("chalk");
 const strpad = require("strpad");
+const cTable = require('console.table');
 const connection = mysql.createConnection({
     host: process.env.host,
     port: process.env.port,
@@ -48,9 +49,10 @@ function departmentSales(){
         if (error){
             throw error;
         }
-        response.forEach(function(item){
-            console.log(chalk`\n ID:|{green ${item.department_id}}| Department:| {green ${strpad.right(item.department_name, 15)}}|Product Sales:| {green ${strpad.right(item.product_sales.toString(),5)}}| Overhead:| {green ${strpad.right(item.over_head_costs.toString(), 5)}} |Total Profit:| {green ${strpad.right(item.total_profit.toString(), 5)}}\n`)
-        })
+        console.table(response)
+        // response.forEach(function(item){
+        //     console.log(chalk`\n ID:|{green ${item.department_id}}| Department:| {green ${strpad.right(item.department_name, 15)}}|Product Sales:| {green ${strpad.right(item.product_sales.toString(),5)}}| Overhead:| {green ${strpad.right(item.over_head_costs.toString(), 5)}} |Total Profit:| {green ${strpad.right(item.total_profit.toString(), 5)}}\n`)
+        // })
     taskMenu()
     })
 }
