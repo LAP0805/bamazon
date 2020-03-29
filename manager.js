f931ea944c251401b0b9c2d165cb72e10381451a
Janxxy <lesley.pond@gmail.com>
2020-03-29 10:41:02 -0400

    formatting code

--- manager.js
+++ manager.js
@@ -10,37 +10,31 @@ const connection = mysql.createConnection({
     password: process.env.password,
     database: process.env.database
 });
-connection.connect(function(error){
-    if (error){
-        throw error;    
+connection.connect(function (error) {
+    if (error) {
+        throw error;
     }
     console.log(`\n connected as id: ${connection.threadId} \n`);
     taskMenu();
 })
 
 
-function taskMenu(){
-    inquirer.prompt([
-        {
-            name: "what",
-            message: "What would you like to do today?",
-            type: "list",
-            choices: ["View products for sale", "View low inventory", "Add to inventory", "Add new product", "Quit"]
-        }
-    ]).then(function(response){
-        if(response.what === "View products for sale"){
+function taskMenu() {
+    inquirer.prompt([{
+        name: "what",
+        message: "What would you like to do today?",
+        type: "list",
+        choices: ["View products for sale", "View low inventory", "Add to inventory", "Add new product", "Quit"]
+    }]).then(function (response) {
+        if (response.what === "View products for sale") {
             showProducts();
-        }
-        else if(response.what === "View low inventory"){
+        } else if (response.what === "View low inventory") {
             lowInventory();
-        }
-        else if(response.what === "Add to inventory"){
+        } else if (response.what === "Add to inventory") {
             addInventory();
-        }
-        else if(response.what === "Add new product"){
+        } else if (response.what === "Add new product") {
             addProduct();
-        }
-        else if(response.what === "Quit"){
+        } else if (response.what === "Quit") {
             console.log("Thank you, Goodbye!")
             connection.end();
         }
@@ -48,101 +42,93 @@ function taskMenu(){
     })
 }
 
-function showProducts(){
-    connection.query("select * from products", function(error,response){
-       if(error) {
-           throw error;
-       }
-     console.log(strpad.right('-', 63, '-'));
-     response.forEach(function(item){
-         console.log(chalk
-             `{green ${strpad.right(item.product_name, 20)}} | ID: {green ${strpad.right(item.item_id.toString(), 2)}} | $ {green ${strpad.right(item.price.toString(), 5)}} | {green ${strpad.right(item.department_name, 12)}} | InStock: {green ${item.stock_quantity}}
+function showProducts() {
+    connection.query("select * from products", function (error, response) {
+        if (error) {
+            throw error;
+        }
+        console.log(strpad.right('-', 63, '-'));
+        response.forEach(function (item) {
+            console.log(chalk `{green ${strpad.right(item.product_name, 20)}} | ID: {green ${strpad.right(item.item_id.toString(), 2)}} | $ {green ${strpad.right(item.price.toString(), 5)}} | {green ${strpad.right(item.department_name, 12)}} | InStock: {green ${item.stock_quantity}}
 `)
-            
-     })
-    taskMenu()
+
+        })
+        taskMenu()
     })
 
 }
 
-function lowInventory(){
-    connection.query(" select * from products where stock_quantity < 10", function(error, response){
-        if (error){
+function lowInventory() {
+    connection.query(" select * from products where stock_quantity < 10", function (error, response) {
+        if (error) {
             throw error
         }
         console.log(`\n Items with low inventory:`)
-        if (!response[0]){
+        if (!response[0]) {
             console.log(`All items are sufficiently stocked!\n`)
+        } else {
+            response.forEach(function (item) {
+                console.log(chalk `\n{green ${strpad.right(item.product_name, 20)}} | ID: {green ${strpad.right(item.item_id.toString(), 2)}} | $ {green ${strpad.right(item.price.toString(), 5)}} | {green ${strpad.right(item.department_name, 12)}} | InStock: {green ${item.stock_quantity}}\n`)
+            })
         }
-        else{
-      response.forEach(function(item){
-              console.log(chalk
-                `\n{green ${strpad.right(item.product_name, 20)}} | ID: {green ${strpad.right(item.item_id.toString(), 2)}} | $ {green ${strpad.right(item.price.toString(), 5)}} | {green ${strpad.right(item.department_name, 12)}} | InStock: {green ${item.stock_quantity}}\n`)
-      })
-    }
-      taskMenu();
+        taskMenu();
     })
 }
 
-function addInventory(){
-        inquirer.prompt([
-            {
-                name: "departmentName",
-                message: "What is the department you would like to add a product to?",
-                type: "input",
-            },
-            {
-                name: "whichProduct",
-                message: "What is the ID of the product you would like to stock?",
-                type: "input",
-                validate: function(input){
-                    if(isNaN(input)){
-                        return false
-                    }
-                    else{
-                        return true
-                    }
-                }
-            },
-            {
-                name:"howMany",
-                message: "How many units would you like to add?",
-                type: "input",
-                validate: function(input){
-                    if(isNaN(input)){
-                        return false
-                    }
-                    else{
-                        return true;
-                    }
+function addInventory() {
+    inquirer.prompt([{
+            name: "departmentName",
+            message: "What is the department you would like to add a product to?",
+            type: "input",
+        },
+        {
+            name: "whichProduct",
+            message: "What is the ID of the product you would like to stock?",
+            type: "input",
+            validate: function (input) {
+                if (isNaN(input)) {
+                    return false
+                } else {
+                    return true
                 }
             }
-        ]).then(function(response){
-            let departmentName = response.departmentName
-            let item = response.whichProduct;
-            let howMany= response.howMany;
-            connection.query("update products set stock_quantity =(stock_quantity +" + howMany +" ) where item_id =" + item, function(error, response){
+        },
+        {
+            name: "howMany",
+            message: "How many units would you like to add?",
+            type: "input",
+            validate: function (input) {
+                if (isNaN(input)) {
+                    return false
+                } else {
+                    return true;
+                }
+            }
+        }
+    ]).then(function (response) {
+        let departmentName = response.departmentName
+        let item = response.whichProduct;
+        let howMany = response.howMany;
+        connection.query("update products set stock_quantity =(stock_quantity +" + howMany + " ) where item_id =" + item, function (error, response) {
+            if (error) {
+                throw error;
+            }
+            connection.query("update supervisor set over_head_costs = over_head_costs + (select sum(products.item_cost *" + howMany + ") from products where item_id =" + item + ") where department_name = '" + departmentName + "'", function (error, response) {
                 if (error) {
                     throw error;
                 }
-                connection.query("update supervisor set over_head_costs = over_head_costs + (select sum(products.item_cost *" + howMany + ") from products where item_id =" + item + ") where department_name = '" + departmentName + "'"
-             ,function(error, response){
-                if (error) {
-                    throw error;
-                }
-            }) 
             })
-            
-            showProducts()
         })
+
+        showProducts()
+    })
 }
 
-function addProduct(){
-    inquirer.prompt([
-        {
+function addProduct() {
+    inquirer.prompt([{
             name: "name",
             message: "Enter product name:",
-            type:"input"
+            type: "input"
         },
         {
             name: "price",
@@ -157,25 +143,25 @@ function addProduct(){
         {
             name: "department",
             message: "Enter product department:",
-            type:"input"
+            type: "input"
         },
         {
             name: "inStock",
             message: "Enter number of items in stock",
             type: "input"
         }
-    ]).then(function(response){
+    ]).then(function (response) {
         let name = response.name;
         let price = response.price;
         let department = response.department;
         let inStock = response.inStock;
-        let cost= response.cost;
-        connection.query("insert into products (product_name, price, department_name, stock_quantity, item_cost) values ('" + name + "','" + price + "','" + department + "','" + inStock + "','" + cost+ "')", function(error, response){
-            if(error) {
+        let cost = response.cost;
+        connection.query("insert into products (product_name, price, department_name, stock_quantity, item_cost) values ('" + name + "','" + price + "','" + department + "','" + inStock + "','" + cost + "')", function (error, response) {
+            if (error) {
                 throw error;
             }
-            connection.query("update supervisor set over_head_costs = over_head_costs + (select sum("+cost +"*"+inStock+" ))  where department_name ='" + department+ "'", function(error, response){
-                if(error) {
+            connection.query("update supervisor set over_head_costs = over_head_costs + (select sum(" + cost + "*" + inStock + " ))  where department_name ='" + department + "'", function (error, response) {
+                if (error) {
                     throw error;
                 }
             })
@@ -183,4 +169,3 @@ function addProduct(){
         })
     })
 }
-
